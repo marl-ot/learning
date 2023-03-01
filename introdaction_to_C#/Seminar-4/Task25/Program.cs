f8d1e7ac32dec09b1e804b4f4100a20ee2e386b5
svyatoslav kalmykov <marlot.swyat@gmail.com>
2023-03-01 22:31:59 +0300

    seminar 4 homework

---
+++ introdaction_to_C#/Seminar-4/Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputValue(string message)
+{
+    Console.Write($"{message} > ");
+    int Value;
+    bool isCorrect = int.TryParse(Console.ReadLine(), out Value);
+    if (isCorrect) {return Value;}
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int A = InputValue("Введите основание числа: ");
+int B = InputValue("Введите степень числа: ");
+
+int Degree(int A, int B)
+{
+    int local_result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        local_result = A*B;
+    }
+    return local_result;
+}
+
+int result = Degree(A, B);
+System.Console.WriteLine($"Число {A} в степени {B} будет равно {result}");
---
+++ introdaction_to_C#/Seminar-4/Task29/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write('[');
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write(']');
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
