46586e750e19af69dcf1b419d16572ae5351e975
svyatoslav kalmykov <marlot.swyat@gmail.com>
2023-03-03 12:51:52 +0300

    seminar-5: homework task1

---
+++ introdaction_to_C#/Seminar-5/Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int InputValue(string message)
+{
+    Console.Write($"{message} > ");
+    int Value;
+    bool isCorrect = int.TryParse(Console.ReadLine(), out Value);
+    if (isCorrect) {return Value;}
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int[] GenerateArray(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write('[');
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.WriteLine(']');
+}
+
+int EvenCount(int[] array)
+{
+    int count = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int length = InputValue("Длина массива: ");
+int[] array = GenerateArray(length);
+int CountEven = EvenCount(array);
+PrintArray(array);
+System.Console.WriteLine($"Количество четныйх элементов в массиве равно {CountEven}");
