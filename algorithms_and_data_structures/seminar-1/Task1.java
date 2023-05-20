a107e2d5cb0a5fd65e2b5ff063801725b5a5cb5b
svyatoslav kalmykov <marlot.swyat@gmail.com>
2023-05-20 21:57:16 +0300

    add algorithms & data structures seminar-1 tasks

--- algorithms_and_data_structures/seminar-1/Task1.java
+++ algorithms_and_data_structures/seminar-1/Task1.java
@@ -1,8 +1,7 @@
-public class Task 1 {
+public class Task1 {
     public static void main(String[] args){
-        System.out.println(findSumOfArray(n:10));
+        System.out.println(findSumOfArray(10));
     }
-
     public static int findSumOfArray(int n){
         int result = 0;
         for (int i = 1; i <= n; i++) result += i;
