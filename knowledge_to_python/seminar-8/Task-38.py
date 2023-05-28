97d7c378cfac423e98fa71e91f2917882ecee72a
svyatoslav kalmykov <marlot.swyat@gmail.com>
2023-05-28 23:09:58 +0300

    add python seminar-8 homework

--- knowledge_to_python/seminar-8/Task-38.py
+++ knowledge_to_python/seminar-8/Task-38.py
@@ -25,43 +25,69 @@ def find_data():
     """Печатает результат поиска по справочнику."""
     with open('book.txt', 'r', encoding='utf-8') as book:
         data = book.read()
-    contact_to_find = input('Введите, что хотите найти: ')
-    result = search(data, contact_to_find)
+    context_to_find = input('Введите, что хотите найти: ')
+    result = search(data, context_to_find)
     return result
 
 
 def search(book, info):
     """Находит в списке записи по определенному критерию поиска"""
     book = book.split('\n')
-    return list(filter(lambda contact: info.lower() in contact.lower(), book))
+    return list(filter(lambda context: info.lower() in context.lower(), book))
 
 
 def replace_data():
     """Удаляет строку с данными в справочнике"""       
-    rd_book = open('book.txt', 'r+', encoding='utf-8')
-    data = rd_book.read()
-    contact_to_change = input('Введите, что хотите заменить: ')
+    rd_book = open('book.txt', 'r', encoding='utf-8')
+    data = show_data()
+    print(data)
+    context_to_change = input('Введите, что хотите заменить: ')
     while True:
-        srch = search(data, contact_to_change)
+        srch = search(data, context_to_change)
         if len(srch) == 1:
-            contact_to_replace = input('Введите то, на что хотите перезаписать: ')
-            data = data.replace(contact_to_change, contact_to_replace)
-            print(data)
-            rd_book.close()
+            context_to_replace = input('Введите то, на что хотите перезаписать: ')
+            data = data.replace(context_to_change, context_to_replace)
             break
         elif len(srch) > 1:
-            contact_to_change = input('Слишком много вхождений. \nСкорректируйте заменяемую информацию: ')
+            context_to_change = input('Слишком много вхождений. \nСкорректируйте заменяемую информацию: ')
         elif len(srch) == 0:
-            contact_to_change = input('Нет вхождений. \nСкорректируйте заменяемую информацию: ')
+            context_to_change = input('Нет вхождений. \nСкорректируйте заменяемую информацию: ')
         else:
             break
-    wrt_book = open('book.txt', 'w', encoding='utf-8').write(data)
-    
-    return wrt_book
+    rd_book.close()
+    wrt_book = open('book.txt', 'w', encoding='utf-8')
+    wrt_book.write(data)
+    wrt_book.close()
+    return data
+
+
+def delete_data():
+    """Удаляет строку с данными в справочнике"""       
+    rd_book = open('book.txt', 'r', encoding='utf-8')
+    data = show_data()
+    context_to_del = input('Введите, что хотите удалить: ')
+    while True:
+        srch = search(data, context_to_del)
+        if len(srch) == 1:
+            data = data.replace(context_to_del, '')
+            break
+        elif len(srch) > 1:
+            context_to_del = input('Слишком много вхождений. \nСкорректируйте заменяемую информацию: ')
+        elif len(srch) == 0:
+            context_to_del = input('Нет вхождений. \nСкорректируйте заменяемую информацию: ')
+        else:
+            break
+
+    rd_book.close()
+    wrt_book = open('book.txt', 'w', encoding='utf-8')
+    wrt_book.write(data)
+    wrt_book.close()
+
+    return data
 
 
 while True:
-    print("1. вывод \n2. добавление \n3. поиск \n4. изменение через поиск")
+    print("1. вывод \n2. добавление \n3. поиск \n4. изменение через поиск \n5. удаление записей \n6. выход")
     mode = int(input("Выберите режим: "))
     if mode == 1:
         print(show_data())
@@ -71,6 +97,10 @@ while True:
         print(find_data())
     elif mode == 4:
         print(replace_data())
+    elif mode == 5:
+        print(delete_data())
+    elif mode == 6:
+        break
     else:
         print('Указан несуществующий режим')
         break
