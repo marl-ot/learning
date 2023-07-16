e2c3a019504a2b73da7067bd6b0d8f9b54daf3ee
svyatoslav kalmykov <marlot.swyat@gmail.com>
2023-07-16 16:51:54 +0300

    add seminar3 homework to OOP_java

--- object_oriented_java_programming/seminar3/EnWordGame.java
+++ object_oriented_java_programming/seminar3/EnWordGame.java
@@ -3,14 +3,11 @@ package object_oriented_java_programming.seminar3;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WordGame extends AbstractGame {
+public class EnWordGame extends AbstractGame {
     @Override
     List<String> generateCharList() {
         List<String> result = new ArrayList<>();
         for (char ch = 'a'; ch <= 'z'; ch++) {
-            result.add(String.valueOf(ch));
-        }
-        for (char ch = 'а'; ch <= 'я'; ch++) {
             result.add(String.valueOf(ch));
         }
         return result;
