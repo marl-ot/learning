6c6367c110212dec18803ba0c61aabe83cc96e08
marl-ot <marlot.swyat@gmail.com>
2024-04-04 13:41:12 +0300

    add HW1 for Django FW

---
+++ Django_framework/PR-1/GB/PR1/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import logging
+
+# Настройка логирования
+logging.basicConfig(filename='access.log', level=logging.INFO)
+
+def home(request):
+    html = """
+    <h1>Добро пожаловать на мой первый Django-сайт!</h1>
+    <p>Это главная страница сайта.</p>
+    <p>Меня зовут, Святослав.</p>
+    """
+    
+    # Запись информации о посещении страницы в лог
+    logging.info('Посещена главная страница')
+    
+    return HttpResponse(html)
+
+def about(request):
+    html = """
+    <h1>Обо мне</h1>
+    <p>Здесь вы можете узнать немного о мне.</p>
+    <p>Меня зовут Святослав.</p>
+    """
+    
+    # Запись информации о посещении страницы в лог
+    logging.info('Посещена страница "О себе"')
+    
+    return HttpResponse(html)
